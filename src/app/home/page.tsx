f4db8a39483d803c4db55b4500eabff978c33a48
Hamza Latif <naqshband.hamza@gmail.com>
2024-10-09 21:54:37 +0500

    chnages 201

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2577,13 +2577,14 @@ export default function LandingPage() {
                         ))}
                     </div>
                     <p id="g-p">The Fastest Growing <span className="span-1">Video</span> Production Company</p>
+
+                </div>
+                <div className="another">
                     <div className="custom-shape-divider-top-1728323637">
                         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                             <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
                         </svg>
                     </div>
-                </div>
-                <div className="another">
                 </div>
                 <div className="another1">
 
