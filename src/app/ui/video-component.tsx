79a7a6a2ff4de6d69fa9679f3080bb5c0463c895
Hamza Latif <naqshband.hamza@gmail.com>
2024-10-09 21:30:33 +0500

    changes

--- src/app/ui/video-component.tsx
+++ src/app/ui/video-component.tsx
@@ -4,6 +4,6 @@ import BackgroundVideo from 'next-video/background-video';
 export default function VideoComponent({ src, w, h, left, top }) {
 
     return (
-        <BackgroundVideo src={src} width={w} height={h} style={{ position: "absolute", top: top, left: left, objectFit: "cover" }}/>
+        <BackgroundVideo src={src} width={w} height={h} style={{ position: "absolute", top: top, left: left, objectFit: "cover" }} />
     );
 }
